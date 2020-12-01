d838eaf877bba85fee8cdf9e5589f8827bda2168
LvJinyu <lvjinyu@myhexin.com>
2020-12-01 20:35:15 +0800

    字符串反转添加注释;

--- lstrings/strings.go
+++ lstrings/strings.go
@@ -1,6 +1,12 @@
+// @Author: lvjinyu
+// @Time: 2020/12/1 19:43
 package lstrings
 
-// 字符串反转
+// @Author lvjinyu
+// @Description 字符串反转
+// @Date 20:11 2020/12/1
+// @Param s string "需要反转的字符串"
+// @return s string "反转后的字符串"
 func ReverseString(s string) string {
 	runes := []rune(s)
 	for from, to := 0, len(s)-1; from < to; from, to = from+1, to-1 {
